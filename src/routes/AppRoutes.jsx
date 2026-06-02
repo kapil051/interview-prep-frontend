afd17ce9157e7601be2ccb2779bfc0f7243e5ac5
QL-Kapil-Kant <kapil.kant@quokkalabs.com>
2026-06-02 12:26:36 +0530

    (fix): add protected routes check

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,14 +3,17 @@ import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
 import DashboardPage from '../pages/DashboardPage';
 import Blind75Page from '../pages/Blind75Page';
+import ProtectedRoute from './ProtectedRoute';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/blind75" element={<Blind75Page />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/blind75" element={<Blind75Page />} />
+      </Route>
       <Route path="/" element={<Navigate to="/login" />} />
     </Routes>
   );
